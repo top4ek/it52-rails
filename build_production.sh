aaa2e90a8356c96c83d1773ed9023fcaad41d205
Nick Kugaevsky <nick@kugaevsky.ru>
2019-10-11 22:21:21 +0300

    ci: move config copy to build script

--- build_production.sh
+++ build_production.sh
@@ -4,6 +4,9 @@ export VCS_REF=$(git rev-parse --short HEAD)
 export BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export VERSION="production"
 
+cp config/database.yml.template config/database.yml
+cp config/application.yml.template config/application.yml
+
 docker-compose -f docker-compose.production.yml pull rails
 docker-compose -f docker-compose.production.yml build rails
 docker-compose -f docker-compose.production.yml push rails
