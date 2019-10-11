e0b218185f2cb39da11878c550a1e24fe99b7692
Nick Kugaevsky <nick@kugaevsky.ru>
2019-10-11 20:24:12 +0300

    ci: unlock and clean up compiled files between builds

--- build_production.sh
+++ build_production.sh
@@ -3,6 +3,6 @@ export VCS_REF=$(git rev-parse --short HEAD)
 export BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 export VERSION="production"
 
-docker-compose -f docker-compose.production.yml pull
+docker-compose -f docker-compose.production.yml pull rails
 docker-compose -f docker-compose.production.yml build rails
 docker-compose -f docker-compose.production.yml push rails
